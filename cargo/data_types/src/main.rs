48f53cc3115d8cb39deac54a6ec61540e23bc949
root <root@pop-os.localdomain>
2022-06-18 13:09:44 +0530

    tups and arrays

---
+++ cargo/data_types/src/main.rs
@@ -0,0 +1,21 @@
+use std::io;
+
+fn main() {
+    //tuple
+    let tup = (500, 0.6 , 0);
+    let five_hundred = tup.0;
+    let point_six = tup.1;
+    let nill = tup.2;
+    println!("The values of five_hundred, point_six and nill are: {}, {} and {}",five_hundred, point_six, nill);
+    //Array
+    let arr: [u32;5] = [4,5,6,7,8];
+    let mut index = String::new();
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Couldn't read line");
+    let index:usize= match index.trim().parse(){
+        Ok(num) => num,
+        Err(_) => 0
+    };
+    println!("Value of element index {} is {}",index, arr[index]);
+}
---
+++ cargo/data_types/target/debug/data_types.d
@@ -0,0 +1,1 @@
+/home/jodis/Desktop/rust/cargo/data_types/target/debug/data_types: /home/jodis/Desktop/rust/cargo/data_types/src/main.rs
