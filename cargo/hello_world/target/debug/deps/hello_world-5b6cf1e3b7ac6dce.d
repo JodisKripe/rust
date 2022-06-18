2381d1fad62400222e811d1fd22bbae76968e6a4
root <root@pop-os.localdomain>
2022-06-18 11:37:56 +0530

    HelloWorld

---
+++ cargo/hello_world/target/debug/deps/hello_world-5b6cf1e3b7ac6dce.d
@@ -0,0 +1,5 @@
+/home/jodis/Desktop/rust/cargo/hello_world/target/debug/deps/hello_world-5b6cf1e3b7ac6dce: src/main.rs
+
+/home/jodis/Desktop/rust/cargo/hello_world/target/debug/deps/hello_world-5b6cf1e3b7ac6dce.d: src/main.rs
+
+src/main.rs:
---
+++ cargo/hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/jodis/Desktop/rust/cargo/hello_world/target/debug/hello_world: /home/jodis/Desktop/rust/cargo/hello_world/src/main.rs
